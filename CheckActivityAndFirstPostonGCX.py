a06f34899c820bf4ad05a9930a94b9cb4c4557c5
insertcustomname <43460834+insertcustomname@users.noreply.github.com>
2019-03-31 15:05:43 +0800

    Update CheckActivityAndFirstPostonGCX.py

--- CheckActivityAndFirstPostonGCX.py
+++ CheckActivityAndFirstPostonGCX.py
@@ -1,11 +1,12 @@
+
 import praw
 import time
 from datetime import datetime
 
-reddit = praw.Reddit(client_id='',
-                     client_secret='',
-                     username='',
-                     password='',
+reddit = praw.Reddit(client_id='dt0u8lacZcydbg',
+                     client_secret='GbjnUAnxbf_4u0SL4RFhkIdLqLE',
+                     username='repbot1234',
+                     password='fasttalker101',
                      user_agent='reputation bot by hacksorskill')
  
 # look for phrase and reply appropriately
@@ -56,26 +57,58 @@ def active(user):
         if count < 30:
             activevar=True
             return(activevar)
+            activevar=True
+            return(activevar)
+
         return(activevar)
     except:
         print('error')
         pass
 
 subreddit=reddit.subreddit('giftcardexchange')
+while True:
+    try:
+        for comment in subreddit.stream.comments():
+            try:
+                if comment.author_flair_text == "GCX Beginner":
+                    g=active(str(comment.author))
+                    if g is True:
+                        reddit.redditor('r/uselessposts').message('Post on GCX Detected, low activity + new user on sub ', "u/" + str(comment.author))
+                    else:
+                        pass
+                    
+                    
+                    
+                else:
+                    pass
+            except Exception as e:
+                print(e)
+    except Exception as e:
+        print(e)
+        pass
 
-try:
-    for comment in subreddit.stream.comments():
-        if comment.author_flair_text == "GCX Beginner":
-            g=active(str(comment.author))
-            if g is True:
-                reddit.redditor('r/uselessposts').message('Post on GCX Detected, low activity + new user on sub ', "u/" + str(comment.author))
-            else:
-                pass
-            
-            
-            
-        else:
-            pass
-except Exception as e:
-    print(e)
-    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
